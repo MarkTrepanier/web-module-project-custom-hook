0334a3a7af2c7433bace3441044c9b3d36683d8a
MarkTrepanier <marktrepanier.cajigas@gmail.com>
2021-09-29 16:20:09 -0400

    uselocalstorage created

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,17 @@
+import {useState} from 'react'
+const useLocalStorage = (key, initialValue)=>{
+
+    const[storedValue, setStoredValue]= useState(()=>{
+        //storedValue is either item or initialValue
+        const item = window.localStorage.getItem(key);
+        //return that
+        return item ? JSON.parse(item) : initialValue;
+    });
+
+    const setValue=value=>{
+        setStoredValue(value);
+        window.localStorage.setItem(key, JSON.stringify(value));
+    }
+
+    return [storedValue,setValue];
+}
